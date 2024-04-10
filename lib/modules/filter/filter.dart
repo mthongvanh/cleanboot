1cba6138a90557207043913d86c1465c822b0aee
Mike Thongvanh <michael@thongvanh.com>
2024-04-10 09:35:02 +0200

    fix(filter): exposed controller and view model classes

--- lib/modules/filter/filter.dart
+++ lib/modules/filter/filter.dart
@@ -1,5 +1,7 @@
 export 'data/data.dart';
 export 'domain/domain.dart';
 export 'filter/filter_page.dart';
+export 'filter/filter_page_controller.dart';
+export 'filter/filter_page_view_model.dart';
 export 'mappers/filter_item_mapper.dart';
 export 'mappers/filter_section_mapper.dart';
