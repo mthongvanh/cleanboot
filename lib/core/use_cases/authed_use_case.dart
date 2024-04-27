cce650a21c1c94f7ac6e8c5c8d0ed0962650ec89
Mike Thongvanh <michael@thongvanh.com>
2024-04-27 15:39:34 +0200

    fix(update_count): fixed view count update for anonymous users

--- lib/core/use_cases/authed_use_case.dart
+++ lib/core/use_cases/authed_use_case.dart
@@ -6,12 +6,20 @@ mixin AuthedUseCase {
   GetAuthedUserUseCase get getAuthedUserUseCase;
 
   /// Execute the use case
-  Future<({Failure? failure, AuthedUser? result})> authedUser() async {
+  Future<({Failure? failure, AuthedUser? result})> authedUser({
+    final bool allowAnonymous = false,
+  }) async {
     AuthFailure? failure;
     AuthedUser? result;
     final response = await getAuthedUserUseCase.execute(NoParams());
-    if (response.result != null && !response.result!.anonymous) {
-      result = response.result;
+    if (response.result != null) {
+      if (allowAnonymous || !response.result!.anonymous) {
+        result = response.result;
+      } else {
+        failure = AuthFailure.invalidCredentials(
+          description: response.failure?.description,
+        );
+      }
     } else {
       failure = AuthFailure.invalidCredentials(
         description: response.failure?.description,
